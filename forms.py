86443b4b2ddf255fdab73e9db799f444a8e746b1
Sasha Kantoriz <no.money.no.honey5151@gmail.com>
2023-01-17 06:10:01 +0200

    Add translation

--- forms.py
+++ forms.py
@@ -4,21 +4,21 @@ from wtforms.widgets import TextArea
 
 
 class BaseMemberForm(FlaskForm):
-    role = SelectField('Role:', choices=[('grand', 'Grand'), ('adult', 'Adult'), ('child', 'Child'), ('grandchild', 'Grandchild')], id='role')
-    gender = RadioField('Gender:', choices=[('male', 'Male'), ('female', 'Female')], id='gender')
-    full_name = StringField('Full Name:', id='full_name')
-    date_of_birth = StringField('Date of Birth:', id='date_of_birth')
-    place_of_birth = StringField('Place of Birth:', id='place_of_birth')
-    complete_address = StringField('Complete Address:', id='complete_address', widget=TextArea())
-    deceased = BooleanField('Deceased:', id='deceased')
+    role = SelectField('Peran:', choices=[('grand', 'Kakek / Nenek'), ('adult', 'Anak'), ('child', 'Cucu'), ('grandchild', 'Cicit')], id='role')
+    gender = RadioField('Jenis Kelamin:', choices=[('male', 'Pria'), ('female', 'Perempuan')], id='gender')
+    full_name = StringField('Nama Lengkap:', id='full_name')
+    date_of_birth = StringField('Tanggal Lahir:', id='date_of_birth')
+    place_of_birth = StringField('Tempat Lahir:', id='place_of_birth')
+    complete_address = StringField('Alamat Lengkap:', id='complete_address', widget=TextArea())
+    deceased = BooleanField('Almarhum:', id='deceased')
     #
-    mother_id = SelectField('Mother:', id='mother_id')
-    father_id = SelectField('Father:', id='father_id')
+    mother_id = SelectField('Nama lengkap Ibu:', id='mother_id')
+    father_id = SelectField('Nama lengkap Ayah:', id='father_id')
     #
-    husbands_full_name = StringField('Husbands Full Name:', id='husbands_full_name')
-    wifes_full_name = StringField('Wifes Full Name:', id='wifes_full_name')
-    inlaws_full_name = StringField('Inlaws Full Name:', id='inlaws_full_name')
-    father_inlaws_full_address = StringField('Father Inlaws Full Address:', id='father_inlaws_full_address', widget=TextArea())
+    husbands_full_name = StringField('Nama lengkap Suami:', id='husbands_full_name')
+    wifes_full_name = StringField('Nama lengkap Istri:', id='wifes_full_name')
+    inlaws_full_name = StringField('Nama lengkap Menantu:', id='inlaws_full_name')
+    father_inlaws_full_address = StringField('Alamat lengkap Mertua:', id='father_inlaws_full_address', widget=TextArea())
 
 
 class ChildMemberForm(BaseMemberForm):
